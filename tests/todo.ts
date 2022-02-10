7acb4305fb79bab71600116436d3ba7d72b4efa4
jackson1992121 <dantes0121@hotmail.com>
2022-02-10 21:08:28 +0800

    initial commit

---
+++ tests/todo.ts
@@ -0,0 +1,82 @@
+import * as anchor from '@project-serum/anchor';
+import { Program } from '@project-serum/anchor';
+import { Todo } from '../target/types/todo';
+import BN = require('bn.js');
+import * as chai from 'chai';
+import NodeWallet from '@project-serum/anchor/dist/cjs/nodewallet';
+
+const { SystemProgram, LAMPORTS_PER_SOL } = anchor.web3;
+const { expect } = chai;
+
+const provider = anchor.Provider.env();
+anchor.setProvider(provider);
+const mainProgram = anchor.workspace.Todo;
+
+describe('todo', () => {
+
+  it('creates a list', async () => {
+    const owner = await createUser();
+    let list = await createList(owner, 'A list');
+
+    expect(list.data.listOwner.toString(), 'List owner is set').equals(owner.key.publicKey.toString());
+    expect(list.data.name, 'List name is set').equals('A list');
+    expect(list.data.lines.length, 'List has no items').equals(0);
+  });
+});
+
+async function getAccountBalance(pubkey) {
+  let account = await provider.connection.getAccountInfo(pubkey);
+  return account?.lamports ?? 0;
+}
+
+function expectBalance(actual, expected, message, slack = 20000) {
+  expect(actual, message).within(expected - slack, expected + slack);
+}
+
+async function createUser(airdropBalance = 1000000000) {
+  airdropBalance = airdropBalance ?? 10 * LAMPORTS_PER_SOL;
+  let user = anchor.web3.Keypair.generate();
+  let sig = await provider.connection.requestAirdrop(user.publicKey, airdropBalance);
+  await provider.connection.confirmTransaction(sig);
+
+  let wallet: NodeWallet = new NodeWallet(user);
+  let userProvider = new anchor.Provider(provider.connection, wallet, provider.opts);
+
+  return {
+    key: user,
+    wallet,
+    provider: userProvider,
+  };
+}
+
+function createUsers(numUsers) {
+  let promises = [];
+  for (let i = 0; i < numUsers; i++) {
+    promises.push(createUser());
+  }
+
+  return Promise.all(promises);
+}
+
+function programForUser(user) {
+  return new anchor.Program(mainProgram.idl, mainProgram.programId, user.provider);
+}
+
+async function createList(owner, name, capacity = 16) {
+  const [listAccount, bump] = await anchor.web3.PublicKey.findProgramAddress(
+    ['todolist', owner.key.publicKey.toBytes(), name.slice(0, 32)],
+    mainProgram.programId
+  );
+
+  let program = programForUser(owner);
+  await program.rpc.newList(name, capacity, bump, {
+    accounts: {
+      list: listAccount,
+      user: owner.key.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+  });
+
+  let list = await program.account.todoList.fetch(listAccount);
+  return { publicKey: listAccount, data: list };
+}
